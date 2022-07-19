d1994495e2a1628b2825dfc0c729997d3c304ce3
Prasanna <prasannac105@gmail.com>
2022-07-19 19:31:31 +0530

    First Commit

---
+++ src/main/java/com/tgt/rysetii/learningresourcesapi/LearningresourcesapiApplication.java
@@ -0,0 +1,15 @@
+package com.tgt.rysetii.learningresourcesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningresourcesapiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LearningresourcesapiApplication.class, args);
+		System.out.println("Hello world....Prasanna");
+	}
+
+}
